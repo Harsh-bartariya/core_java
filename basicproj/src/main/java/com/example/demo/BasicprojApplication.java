8c7200db6feb69a37300b0f467324990c7fbdb13
harsh-bartariya <harshmachine786war@gmail.com>
2025-10-18 00:47:29 +0530

    First commit from STS

---
+++ basicproj/src/main/java/com/example/demo/BasicprojApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicprojApplication.class, args);
+		
+		System.out.println("Hii, i've started now!!");
+	}
+
+}
